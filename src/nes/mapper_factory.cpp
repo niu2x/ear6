9680b151729cdb08bf668bcc1738e355a640e5af
niu2x <niu2x@icloud.com>
2026-05-28 10:24:23 +0800

    build: register mappers 032, 033, 065 in factory and cmake

--- src/nes/mapper_factory.cpp
+++ src/nes/mapper_factory.cpp
@@ -12,6 +12,8 @@
 #include "mapper_013.h"
 #include "mapper_015.h"
 #include "mapper_016.h"
+#include "mapper_032.h"
+#include "mapper_033.h"
 #include "mapper_034.h"
 #include "mapper_038.h"
 #include "mapper_039.h"
@@ -30,6 +32,7 @@
 #include "mapper_093.h"
 #include "mapper_094.h"
 #include "mapper_061.h"
+#include "mapper_065.h"
 #include "mapper_133.h"
 #include "mapper_140.h"
 #include "mapper_143.h"
@@ -48,7 +51,8 @@ bool MapperFactory::is_supported(int mapper_number) {
     switch (mapper_number) {
         case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
         case 8: case 9: case 10: case 11: case 13: case 15: case 16: case 17:
-        case 34: case 38: case 39: case 58: case 60: case 61: case 62:
+        case 32: case 33: case 34: case 38: case 39: case 58: case 60: case 61: case 62:
+        case 65:
         case 66: case 67: case 70: case 78: case 79:
         case 86: case 87: case 89: case 92: case 93: case 94:
         case 101: case 113: case 133: case 140: case 143: case 144:
@@ -80,10 +84,13 @@ BaseMapper* MapperFactory::create(int mapper_number) {
         case 34: return new Mapper034();
         case 38: return new Mapper038();
         case 39: return new Mapper039();
+        case 32: return new Mapper032();
+        case 33: return new Mapper033();
         case 58: return new Mapper058();
         case 60: return new Mapper060();
         case 61: return new Mapper061();
         case 62: return new Mapper062();
+        case 65: return new Mapper065();
         case 66: return new Mapper066();
         case 67: return new Mapper067();
         case 70: return new Mapper070();

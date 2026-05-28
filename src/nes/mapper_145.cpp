768730f7677cc0f2e9513103ae17bafef46e006e
niu2x <niu2x@icloud.com>
2026-05-28 10:22:25 +0800

    feat: implement mappers 145, 148, 149 (Sachen) - simple CHR-only banking

---
+++ src/nes/mapper_145.cpp
@@ -0,0 +1,18 @@
+#include "mapper_145.h"
+
+namespace ear6::nes {
+
+void Mapper145::init(const RomInfo& info, const std::vector<uint8_t>& prg_rom, const std::vector<uint8_t>& chr_rom) {
+    rom_info_ = info; prg_rom_ = prg_rom; chr_rom_ = chr_rom;
+    prg_size_ = (uint32_t)prg_rom.size(); chr_rom_size_ = (uint32_t)chr_rom.size();
+    set_mirroring_type(info.mirroring);
+    add_register_range(0x4100, 0x7FFF, MemoryOperation::WRITE);
+    select_prg_page(0, 0);
+}
+
+void Mapper145::write_register(uint16_t addr, uint8_t value) {
+    if ((addr & 0x4100) != 0x4100) return;
+    select_chr_page(0, (value >> 7) & 0x01);
+}
+
+} // namespace ear6::nes

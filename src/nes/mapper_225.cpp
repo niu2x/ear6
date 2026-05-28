0b9a4de7210387c9c154dded6989a2217bc17bbd
niu2x <niu2x@icloud.com>
2026-05-28 10:27:53 +0800

    feat: implement mapper 225 (Unlicensed multi-cart) - addr-based PRG/CHR with dual mode

---
+++ src/nes/mapper_225.cpp
@@ -0,0 +1,28 @@
+#include "mapper_225.h"
+
+namespace ear6::nes {
+
+void Mapper225::init(const RomInfo& info, const std::vector<uint8_t>& prg_rom, const std::vector<uint8_t>& chr_rom) {
+    rom_info_ = info; prg_rom_ = prg_rom; chr_rom_ = chr_rom;
+    prg_size_ = (uint32_t)prg_rom.size(); chr_rom_size_ = (uint32_t)chr_rom.size();
+    set_mirroring_type(info.mirroring);
+    add_register_range(0x8000, 0xFFFF, MemoryOperation::WRITE);
+    select_prg_page(0, 0); select_prg_page(1, 1); select_chr_page(0, 0);
+}
+
+void Mapper225::write_register(uint16_t addr, uint8_t value) {
+    (void)value;
+    uint8_t high_bit = (addr >> 8) & 0x40;
+    uint8_t prg_page = ((addr >> 6) & 0x3F) | high_bit;
+    if (addr & 0x1000) {
+        select_prg_page(0, prg_page);
+        select_prg_page(1, prg_page);
+    } else {
+        select_prg_page(0, prg_page & 0xFE);
+        select_prg_page(1, (prg_page & 0xFE) + 1);
+    }
+    select_chr_page(0, (addr & 0x3F) | high_bit);
+    set_mirroring_type((addr & 0x2000) ? MirroringType::HORIZONTAL : MirroringType::VERTICAL);
+}
+
+} // namespace ear6::nes

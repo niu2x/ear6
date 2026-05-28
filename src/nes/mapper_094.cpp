4911c522705ddc95da5177f5667578fd7580f255
niu2x <niu2x@icloud.com>
2026-05-28 09:55:29 +0800

    feat: implement mapper 094 (UnROM 1-screen) - UNROM variant with 3-bit PRG banking

---
+++ src/nes/mapper_094.cpp
@@ -0,0 +1,29 @@
+#include "mapper_094.h"
+
+namespace ear6::nes {
+
+void Mapper094::init(const RomInfo& info,
+                     const std::vector<uint8_t>& prg_rom,
+                     const std::vector<uint8_t>& chr_rom) {
+    rom_info_ = info;
+    prg_rom_ = prg_rom;
+    chr_rom_ = chr_rom;
+    prg_size_ = (uint32_t)prg_rom.size();
+    chr_rom_size_ = (uint32_t)chr_rom.size();
+
+    set_mirroring_type(info.mirroring);
+
+    add_register_range(0x8000, 0xFFFF, MemoryOperation::WRITE);
+
+    uint16_t last_bank = (prg_size_ / 0x4000) - 1;
+    select_prg_page(0, 0);
+    select_prg_page(1, last_bank);
+    select_chr_page(0, 0);
+}
+
+void Mapper094::write_register(uint16_t addr, uint8_t value) {
+    (void)addr;
+    select_prg_page(0, (value >> 2) & 0x07);
+}
+
+} // namespace ear6::nes
--- src/nes/mapper_factory.cpp
+++ src/nes/mapper_factory.cpp
@@ -13,6 +13,7 @@
 #include "mapper_015.h"
 #include "mapper_016.h"
 #include "mapper_066.h"
+#include "mapper_094.h"
 #include <cstdio>
 
 namespace ear6::nes {
@@ -39,6 +40,7 @@ bool MapperFactory::is_supported(int mapper_number) {
         case 159:
         case 17:
         case 66:
+        case 94:
         case 7:
             return true;
         default:
@@ -86,6 +88,8 @@ BaseMapper* MapperFactory::create(int mapper_number) {
             return new Mapper006();
         case 66:
             return new Mapper066();
+        case 94:
+            return new Mapper094();
         case 7:
             return new Mapper007();
         default:

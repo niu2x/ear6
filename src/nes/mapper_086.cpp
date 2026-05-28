11c50b9f7b1b5c78e87b120ce9439c102464a5f0
niu2x <niu2x@icloud.com>
2026-05-28 09:59:03 +0800

    feat: implement mapper 086 (Jaleco JF-13) - PRG+CHR banking with audio stub at 000

---
+++ src/nes/mapper_086.cpp
@@ -0,0 +1,31 @@
+#include "mapper_086.h"
+
+namespace ear6::nes {
+
+void Mapper086::init(const RomInfo& info,
+                     const std::vector<uint8_t>& prg_rom,
+                     const std::vector<uint8_t>& chr_rom) {
+    rom_info_ = info;
+    prg_rom_ = prg_rom;
+    chr_rom_ = chr_rom;
+    prg_size_ = (uint32_t)prg_rom.size();
+    chr_rom_size_ = (uint32_t)chr_rom.size();
+
+    set_mirroring_type(info.mirroring);
+    add_register_range(0x6000, 0x7FFF, MemoryOperation::WRITE);
+
+    select_prg_page(0, 0);
+}
+
+void Mapper086::write_register(uint16_t addr, uint8_t value) {
+    switch (addr & 0x7000) {
+        case 0x6000:
+            select_prg_page(0, (value & 0x30) >> 4);
+            select_chr_page(0, (value & 0x03) | ((value >> 4) & 0x04));
+            break;
+        case 0x7000:
+            break;
+    }
+}
+
+} // namespace ear6::nes

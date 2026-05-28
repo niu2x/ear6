7ec8f8ce0f3d5b2f64619187fb5c406f0248909d
niu2x <niu2x@icloud.com>
2026-05-28 10:46:16 +0800

    feat: implement mapper 216 (unlicensed) - addr-based 1-bit PRG/3-bit CHR

---
+++ src/nes/mapper_216.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "base_mapper.h"
+
+namespace ear6::nes {
+
+class Mapper216 : public BaseMapper {
+public:
+    void init(const RomInfo& info, const std::vector<uint8_t>& prg_rom, const std::vector<uint8_t>& chr_rom) override;
+    uint16_t get_prg_page_size() override { return 0x8000; }
+    uint16_t get_chr_page_size() override { return 0x2000; }
+    uint8_t read_register(uint16_t addr) override;
+protected:
+    void write_register(uint16_t addr, uint8_t value) override;
+};
+
+} // namespace ear6::nes

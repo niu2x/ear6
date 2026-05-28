50aed71cf26c007086602954e72d988055549696
niu2x <niu2x@icloud.com>
2026-05-28 10:22:34 +0800

    build: register mappers 061, 133, 143, 145, 148, 149, 156 in factory and cmake

--- src/nes/mapper_factory.cpp
+++ src/nes/mapper_factory.cpp
@@ -29,7 +29,14 @@
 #include "mapper_092.h"
 #include "mapper_093.h"
 #include "mapper_094.h"
+#include "mapper_061.h"
+#include "mapper_133.h"
 #include "mapper_140.h"
+#include "mapper_143.h"
+#include "mapper_145.h"
+#include "mapper_148.h"
+#include "mapper_149.h"
+#include "mapper_156.h"
 #include "mapper_180.h"
 #include "mapper_184.h"
 #include "mapper_185.h"
@@ -41,11 +48,12 @@ bool MapperFactory::is_supported(int mapper_number) {
     switch (mapper_number) {
         case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
         case 8: case 9: case 10: case 11: case 13: case 15: case 16: case 17:
-        case 34: case 38: case 39: case 58: case 60: case 62:
+        case 34: case 38: case 39: case 58: case 60: case 61: case 62:
         case 66: case 67: case 70: case 78: case 79:
         case 86: case 87: case 89: case 92: case 93: case 94:
-        case 101: case 113: case 140: case 144: case 146:
-        case 153: case 157: case 159:
+        case 101: case 113: case 133: case 140: case 143: case 144:
+        case 145: case 146: case 148: case 149:
+        case 153: case 156: case 157: case 159:
         case 180: case 184: case 185:
             return true;
         default:
@@ -74,6 +82,7 @@ BaseMapper* MapperFactory::create(int mapper_number) {
         case 39: return new Mapper039();
         case 58: return new Mapper058();
         case 60: return new Mapper060();
+        case 61: return new Mapper061();
         case 62: return new Mapper062();
         case 66: return new Mapper066();
         case 67: return new Mapper067();
@@ -86,7 +95,13 @@ BaseMapper* MapperFactory::create(int mapper_number) {
         case 92: return new Mapper092();
         case 93: return new Mapper093();
         case 94: return new Mapper094();
+        case 133: return new Mapper133();
         case 140: return new Mapper140();
+        case 143: return new Mapper143();
+        case 145: return new Mapper145();
+        case 148: return new Mapper148();
+        case 149: return new Mapper149();
+        case 156: return new Mapper156();
         case 180: return new Mapper180();
         case 184: return new Mapper184();
         case 185: return new Mapper185();
